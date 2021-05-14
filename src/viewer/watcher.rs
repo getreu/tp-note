5ffc6f6096e89380fe59003fe188629c5ab8a2a9
Jens Getreu <getreu@getreu@web.de>
2021-05-14 10:13:28 +0200

    rollback commit ad6c1c2

--- src/viewer/watcher.rs
+++ src/viewer/watcher.rs
@@ -12,46 +12,87 @@ use std::sync::{Arc, Mutex};
 use std::thread::sleep;
 use std::time::Duration;
 
-/// Even if there is no file modification, after `WATCHER_TIMEOUT` seconds,
-/// the watcher sends an `update` request to check if there are still
-/// subscribers connected.
-const WATCHER_TIMEOUT: u64 = 30;
-
 /// Some file editors do not modify the file on disk, they move the old version
 /// away and write a new file with the same name. As this takes some time,
 /// the watcher waits a bit before trying to access the new file.
 /// The delay is in milliseconds.
 const WAIT_EDITOR_WRITING_NEW_FILE: u64 = 200;
 
+#[derive(Debug)]
+/// Object describing how `State` transitions will happen.  State changes can
+/// take place only each tick.
+pub enum Mode {
+    /// Next state is `State::Started`, the following state will be `State::Blocking`.
+    OneTick(u64),
+    /// Next state is `State::Ticking`.
+    Ticks(u64),
+    /// Next state is `State::Blocking.
+    #[allow(dead_code)]
+    Blocking,
+}
+
+/// State of the watcher. It determines if and when extra `update()` events will
+/// be sent to the web browser in order to check if it is still connected.
+#[derive(Debug)]
+pub enum State {
+    /// Starting state. After next tick goes into `Blocking`
+    Started(u64),
+    /// Send periodic extra `update()` events to the web browser.
+    /// The parameter is the time interval in seconds between ticks.
+    /// If `Mode` does not change, the state remains `Ticking`.
+    Ticking(u64),
+    /// In `Blocking` state, `update()` events are only sent to the web browser
+    /// when the observed file changes. No extra `update()` events are sent.
+    /// If `Mode` does not change, the state remains `Blocking`.
+    Blocking,
+}
+
 /// The `watcher` notifies about changes through `rx`.
-pub struct FileWatcher {
+pub struct FileWatcher<State> {
     /// Receiver for file changed messages.
     rx: Receiver<DebouncedEvent>,
     /// File watcher.
     watcher: RecommendedWatcher,
     /// List of subscribers to inform when the file is changed.
     event_tx_list: Arc<Mutex<Vec<Sender<()>>>>,
+    /// `Mode` object describing how `state` transitions happen.
+    mode: Arc<Mutex<Mode>>,
+    /// State.
+    state: State,
 }
 
 /// Watch file changes and notify subscribers.
-impl FileWatcher {
+impl FileWatcher<State> {
     /// Constructor. `file` is the file to watch.
     pub fn new(
         file: PathBuf,
         event_tx_list: Arc<Mutex<Vec<Sender<()>>>>,
+        mode: Arc<Mutex<Mode>>,
     ) -> Result<Self, ViewerError> {
         let notify_period = CFG.viewer_notify_period;
         let (tx, rx) = channel();
         let mut watcher = watcher(tx, Duration::from_millis(notify_period))?;
         watcher.watch(&file, RecursiveMode::Recursive)?;
 
-        log::debug!("File watcher started.");
+        let state = match *mode.lock().unwrap() {
+            Mode::OneTick(interval) => State::Started(interval),
+            Mode::Ticks(interval) => State::Ticking(interval),
+            Mode::Blocking => State::Blocking,
+        };
+
+        log::debug!(
+            "File watcher started (mode: {:?}, state: {:?})",
+            *mode.lock().unwrap(),
+            state
+        );
 
         Ok(Self {
             rx,
             watcher,
             event_tx_list,
             // By default, tick only once.
+            mode,
+            state,
         })
     }
 
@@ -70,21 +111,32 @@ impl FileWatcher {
     fn run2(&mut self) -> Result<(), ViewerError> {
         loop {
             // Wait for file modifications.
-            let evnt = match self.rx.recv_timeout(Duration::from_secs(WATCHER_TIMEOUT)) {
-                Ok(ev) => ev,
-                Err(RecvTimeoutError::Timeout) => {
-                    // Send subscriber an update event in order to check if they
-                    // are still connected.
-                    self.update()?;
-                    continue;
+            let evnt = match self.state {
+                State::Started(interval) | State::Ticking(interval) => {
+                    match self.rx.recv_timeout(Duration::from_secs(interval)) {
+                        Ok(ev) => ev,
+                        Err(RecvTimeoutError::Timeout) => {
+                            // State transition:
+                            self.state = match *self.mode.lock().unwrap() {
+                                Mode::OneTick(_) => State::Blocking,
+                                Mode::Ticks(interval) => State::Ticking(interval),
+                                Mode::Blocking => State::Blocking,
+                            };
+                            // Send subscriber an update event in order to check if they
+                            // are still connected.
+                            self.update()?;
+                            continue;
+                        }
+                        // The sending half of a channel (or sync_channel) is `Disconnected`,
+                        // implies that no further messages will ever be received.
+                        // As this should never happen, we panic this thread then.
+                        Err(RecvTimeoutError::Disconnected) => panic_any(()),
+                    }
                 }
-                // The sending half of a channel (or sync_channel) is `Disconnected`,
-                // implies that no further messages will ever be received.
-                // As this should never happen, we panic this thread then.
-                Err(RecvTimeoutError::Disconnected) => panic_any(()),
+                State::Blocking => self.rx.recv().unwrap(),
             };
 
-            log::trace!("File watcher event: {:?}", evnt);
+            log::trace!("File watcher ({:?}) event: {:?}", self.state, evnt);
 
             match evnt {
                 DebouncedEvent::NoticeRemove(path) | DebouncedEvent::Remove(path) => {
@@ -125,7 +177,9 @@ impl FileWatcher {
         *tx_list = tx_list.drain(..).filter(|tx| tx.send(()).is_ok()).collect();
 
         log::debug!(
-            "File watcher `update()`: {} subscribers updated.",
+            "File watcher (mode: {:?}, state: {:?}) `update()`: {} subscribers updated.",
+            *self.mode.lock().unwrap(),
+            self.state,
             tx_list.len()
         );
 
