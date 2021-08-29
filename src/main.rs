a2dcf147dbddefed1f0ee17e7397664d1043c06e
Jens Getreu <getreu@getreu@web.de>
2021-08-29 05:39:16 +0200

    update minimum config file version

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 ///    const MIN_CONFIG_FILE_VERSION: Option<&'static str> = None;
 ///    ```
 ///
-const MIN_CONFIG_FILE_VERSION: Option<&'static str> = Some("1.13.3");
+const MIN_CONFIG_FILE_VERSION: Option<&'static str> = VERSION;
 /// (c) Jens Getreu
 const AUTHOR: &str = "(c) Jens Getreu, 2020-2021";
 
