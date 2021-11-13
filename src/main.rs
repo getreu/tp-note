getreu/tp-note

commit 6f14699de55f8cea0d13d3d5fe0ed35ce788a020

Move "empty clipboard" code up to main()

src/main.rs
@@ -35,13 +35,19 @@ use crate::config::backup_config_file;
 use crate::config::CFG;
 use crate::config::CFG_FILE_LOADING;
 use crate::config::CONFIG_PATH;
+use crate::error::WorkflowError;
 use crate::logger::AppLogger;
 use crate::settings::ARGS;
+use crate::settings::LAUNCH_EDITOR;
 #[cfg(feature = "message-box")]
 use crate::settings::RUNS_ON_CONSOLE;
 use crate::workflow::run;
 use chrono::Datelike;
 use chrono::Utc;
+#[cfg(feature = "read-clipboard")]
+use clipboard::ClipboardContext;
+#[cfg(feature = "read-clipboard")]
+use clipboard::ClipboardProvider;
 use error::FileError;
 use semver::Version;
 use serde::Serialize;
@@ -215,16 +221,15 @@ fn main() {
     };
 
     // Run Tp-Note.
-    match run() {
-        Err(e) => {
-            // Something went wrong.
+    let res = run();
+    match res {
+        Err(ref e) => {
+            // Something went wrong. Inform user.
             log::error!("{}", e);
-            AppLogger::flush();
-            process::exit(1);
         }
 
         // Print `path` unless `--export=-`.
-        Ok(path) => {
+        Ok(ref path) => {
             if let Some(p) = &ARGS.export {
                 if p.as_os_str().to_str().unwrap_or_default() != "-" {
                     println!("{}", path.to_str().unwrap_or_default());
@@ -237,4 +242,22 @@ fn main() {
 
     // Wait if there are still error messages windows open.
     AppLogger::flush();
+
+    // Delete clipboard content.
+    #[cfg(feature = "read-clipboard")]
+    if (*LAUNCH_EDITOR
+        && CFG.clipboard.read_enabled
+        && CFG.clipboard.empty_enabled
+        && !*RUNS_ON_CONSOLE)
+        || matches!(&res, Err(WorkflowError::InvalidClipboardYaml { .. }))
+    {
+        let ctx: Option<ClipboardContext> = ClipboardProvider::new().ok();
+        if let Some(mut ctx) = ctx {
+            ctx.set_contents("".to_owned()).unwrap_or_default();
+        };
+    }
+
+    if res.is_err() {
+        process::exit(1);
+    }
 }

src/workflow.rs
@@ -19,10 +19,6 @@ use crate::settings::RUNS_ON_CONSOLE;
 use crate::settings::STDIN;
 #[cfg(feature = "viewer")]
 use crate::viewer::launch_viewer_thread;
-#[cfg(feature = "read-clipboard")]
-use clipboard::ClipboardContext;
-#[cfg(feature = "read-clipboard")]
-use clipboard::ClipboardProvider;
 use std::env;
 use std::fs;
 #[cfg(not(target_family = "windows"))]
@@ -385,15 +381,6 @@ pub fn run() -> Result<PathBuf, WorkflowError> {
                 }
             }
         };
-
-        // Delete clipboard content.
-        #[cfg(feature = "read-clipboard")]
-        if CFG.clipboard.read_enabled && CFG.clipboard.empty_enabled && !*RUNS_ON_CONSOLE {
-            let ctx: Option<ClipboardContext> = ClipboardProvider::new().ok();
-            if let Some(mut ctx) = ctx {
-                ctx.set_contents("".to_owned()).unwrap_or_default();
-            };
-        }
     } else {
         #[cfg(feature = "viewer")]
         if let Some(jh) = viewer_join_handle {

commit 40f2eb8dc1178d7255ad153235a1e0c23988d7bf

Fix imports for some feature only cond. compil.
This bug was introduced with commit 6f14699.

src/main.rs
@@ -35,11 +35,13 @@ use crate::config::backup_config_file;
 use crate::config::CFG;
 use crate::config::CFG_FILE_LOADING;
 use crate::config::CONFIG_PATH;
+#[cfg(feature = "read-clipboard")]
 use crate::error::WorkflowError;
 use crate::logger::AppLogger;
 use crate::settings::ARGS;
+#[cfg(feature = "read-clipboard")]
 use crate::settings::LAUNCH_EDITOR;
-#[cfg(feature = "message-box")]
+#[cfg(any(feature = "read-clipboard", feature = "message-box"))]
 use crate::settings::RUNS_ON_CONSOLE;
 use crate::workflow::run;
 use chrono::Datelike;

src/workflow.rs
@@ -14,7 +14,6 @@ use crate::settings::ARGS;
 use crate::settings::CLIPBOARD;
 use crate::settings::LAUNCH_EDITOR;
 use crate::settings::LAUNCH_VIEWER;
-#[cfg(feature = "read-clipboard")]
 use crate::settings::STDIN;
 #[cfg(feature = "viewer")]
 use crate::viewer::launch_viewer_thread;