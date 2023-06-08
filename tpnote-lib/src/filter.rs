getreu/tp-note

commit 9337c1d61d4d5d5967b29e606f613e9adea4eece

Add env var `TPNOTE_LANG_DETECTION`

tpnote-lib/src/settings.rs
@@ -16,13 +16,19 @@ use windows_sys::Win32::Globalization::GetUserDefaultLocaleName;
 use windows_sys::Win32::System::SystemServices::LOCALE_NAME_MAX_LENGTH;
 
 /// Name of the environment variable, that can be optionally
-/// used to overwrite the user's default language setting.
-/// This is used in various templates.
+/// used to overwrite the user's default language setting, which is
+/// accessible as `{{ lang }}` template variable and used in various
+/// templates.
 pub const ENV_VAR_TPNOTE_LANG: &str = "TPNOTE_LANG";
 
 /// Name of the environment variable, that can be optionally
-/// used to overwrite the user's login name.
-/// This is used in various templates.
+/// used to overwrite the user's `filter_get_lang` and `filter_map_lang`
+/// configuration file setting.
+pub const ENV_VAR_TPNOTE_LANG_DETECTION: &str = "TPNOTE_LANG_DETECTION";
+
+/// Name of the environment variable, that can be optionally
+/// used to overwrite the user's login name. The result is accessible as
+/// `{{ username }}` template variable and used in various templates.
 pub const ENV_VAR_TPNOTE_USER: &str = "TPNOTE_USER";
 
 /// Name of the `LOGNAME` environment variable.
@@ -79,6 +85,7 @@ pub fn update_settings() -> Result<(), ConfigError> {
     update_lang_setting(&mut settings);
     update_filter_get_lang_setting(&mut settings);
     update_filter_map_lang_hmap_setting(&mut settings);
+    update_env_lang_detection(&mut settings);
 
     log::trace!("`SETTINGS` updated:\n{:#?}", settings);
 
@@ -253,4 +260,99 @@ fn update_filter_get_lang_setting(settings: &mut RwLockWriteGuard<Settings>) {
 /// Reset to empty default.
 fn update_filter_get_lang_setting(settings: &mut RwLockWriteGuard<Settings>) {
     let _ = mem::replace(&mut settings.filter_get_lang, Ok(vec![]));
+}
+
+/// Reads the environment variable `LANG_DETECTION`. If not empty,
+/// parse the  content and overwrite `settings.filter_get_lang` and
+/// `settings.filter_map_lang`.
+fn update_env_lang_detection(settings: &mut RwLockWriteGuard<Settings>) {
+    if let Ok(env_var) = env::var(ENV_VAR_TPNOTE_LANG_DETECTION) {
+        if env_var == "" {
+            let _ = mem::replace(&mut settings.filter_get_lang, Ok(vec![]));
+            let _ = mem::replace(&mut settings.filter_map_lang_hmap, None);
+            log::info!(
+                "Empty env. var. `{}` disables `lang-detection` feature.",
+                ENV_VAR_TPNOTE_LANG_DETECTION
+            );
+            return;
+        }
+
+        // Read and convert ISO codes from config object.
+        let mut hm: HashMap<String, String> = HashMap::new();
+        match env_var
+            // The happy path.
+            .split(',')
+            .map(|t| {
+                let t = t.trim();
+                if let Some((lang_subtag, _)) = t.split_once('-') {
+                    // Do not overwrite existing languages.
+                    if !lang_subtag.is_empty() && !hm.contains_key(lang_subtag) {
+                        hm.insert(lang_subtag.to_string(), t.to_string());
+                    };
+                    lang_subtag
+                } else {
+                    t
+                }
+            })
+            // The error path.
+            .map(|l| {
+                IsoCode639_1::from_str(l.trim()).map_err(|_| {
+                    // Produce list of all available langugages.
+                    let mut all_langs = lingua::Language::all()
+                        .iter()
+                        .map(|l| {
+                            let mut s = l.iso_code_639_1().to_string();
+                            s.push_str(", ");
+                            s
+                        })
+                        .collect::<Vec<String>>();
+                    all_langs.sort();
+                    let mut all_langs = all_langs.into_iter().collect::<String>();
+                    all_langs.truncate(all_langs.len() - ", ".len());
+                    // Insert data into error object.
+                    ConfigError::ParseLanguageCode {
+                        language_code: l.into(),
+                        all_langs,
+                    }
+                })
+            })
+            .collect::<Result<Vec<IsoCode639_1>, ConfigError>>()
+        {
+            Ok(mut iso_codes) => {
+                // Add the user's language subtag as reported from the OS.
+                // Continue the happy path.
+                if !settings.lang.is_empty() {
+                    if let Some(lang_subtag) = settings.lang.split('-').next() {
+                        if let Ok(iso_code) = IsoCode639_1::from_str(lang_subtag) {
+                            if !iso_codes.contains(&iso_code) {
+                                iso_codes.push(iso_code);
+                            }
+                            // Check if there is a remainder (region code).
+                            if lang_subtag != settings.lang && !hm.contains_key(lang_subtag) {
+                                hm.insert(lang_subtag.to_string(), settings.lang.to_string());
+                            }
+                        }
+                    }
+                }
+                // Store result.
+                let _ = mem::replace(&mut settings.filter_get_lang, Ok(iso_codes));
+                let _ = mem::replace(&mut settings.filter_map_lang_hmap, Some(hm));
+            }
+            Err(e) =>
+            // Store error.
+            {
+                let _ = mem::replace(&mut settings.filter_get_lang, Err(e));
+            }
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_update_env_lang_detection() {
+        todo!();
+    }
 }

tpnote-lib/src/error.rs
@@ -241,9 +241,10 @@ pub enum ConfigError {
     /// `tmpl.filter_get_lang` and make sure that they are supported, by
     /// checking `tpnote -V`.
     #[error(
-        "The ISO 639-1 language code `{language_code}` in the configuration\n\
-         file variable `tmpl.filter_get_lang` is not supported.\n\
-         The listed codes must be a subset of:\n\
+        "The ISO 639-1 language subtag `{language_code}` in the configuration \
+         file variable `tmpl.filter_get_lang` or in the environment variable \
+         `TPNOTE_LANG_DETECTION` is not supported.\n\
+         All listed codes must be part of the set:\n\
          {all_langs}."
     )]
     ParseLanguageCode {

commit 3f8a7a4a7dedffc707c3e620977b08037c4eee87

Fix: `tpnote_lib` build with `--no-default-features`
This fixes a bug introduced with commit `9337c1d` that broke the build
of `tpnote_lib` when the `lang-detection` feature is not included.

tpnote-lib/src/filter.rs
@@ -2,6 +2,7 @@
 use crate::config::FILENAME_DOTFILE_MARKER;
 use crate::config::LIB_CFG;
 use crate::filename::NotePath;
+#[cfg(feature = "lang-detection")]
 use crate::settings::FilterGetLang;
 use crate::settings::SETTINGS;
 use lazy_static::lazy_static;

tpnote-lib/src/settings.rs
@@ -4,13 +4,17 @@
 //! changes in the related environment variables.
 
 use crate::config::LIB_CFG;
+#[cfg(feature = "lang-detection")]
 use crate::config::TMPL_FILTER_GET_LANG_ALL;
 use crate::error::ConfigError;
+#[cfg(feature = "lang-detection")]
 use lingua;
 #[cfg(feature = "lang-detection")]
 use lingua::IsoCode639_1;
 use std::collections::BTreeMap;
-use std::{env, mem, str::FromStr, sync::RwLock};
+#[cfg(feature = "lang-detection")]
+use std::str::FromStr;
+use std::{env, mem, sync::RwLock};
 #[cfg(target_family = "windows")]
 use windows_sys::Win32::Globalization::GetUserDefaultLocaleName;
 #[cfg(target_family = "windows")]
@@ -49,6 +53,7 @@ const ENV_VAR_USER: &str = "USER";
 const ENV_VAR_LANG: &str = "LANG";
 
 #[derive(Debug)]
+#[allow(dead_code)]
 /// Indicates how the `get_lang` filter operates.
 pub(crate) enum FilterGetLang {
     /// The filter is disabled and returns the empty string.
@@ -240,6 +245,7 @@ fn update_filter_get_lang_setting(settings: &mut Settings) {
         })
         .map(|l| {
             IsoCode639_1::from_str(l).map_err(|_| {
+                // The error path.
                 // Produce list of all available langugages.
                 let mut all_langs = lingua::Language::all()
                     .iter()
@@ -261,6 +267,7 @@ fn update_filter_get_lang_setting(settings: &mut Settings) {
         })
         .collect::<Result<Vec<IsoCode639_1>, ConfigError>>()
     {
+        // The happy path.
         Ok(mut iso_codes) => {
             if all_languages_selected {
                 // Store result.
@@ -294,6 +301,7 @@ fn update_filter_get_lang_setting(settings: &mut Settings) {
                 }
             }
         }
+        // The error path.
         Err(e) =>
         // Store error.
         {
@@ -303,7 +311,7 @@ fn update_filter_get_lang_setting(settings: &mut Settings) {
 }
 
 #[cfg(not(feature = "lang-detection"))]
-/// Reset to empty default.
+/// Disable filter.
 fn update_filter_get_lang_setting(settings: &mut Settings) {
     let _ = mem::replace(&mut settings.filter_get_lang, FilterGetLang::Disabled);
 }
@@ -335,6 +343,7 @@ fn update_filter_map_lang_btmap_setting(settings: &mut Settings) {
 /// Reads the environment variable `LANG_DETECTION`. If not empty,
 /// parse the content and overwrite the `settings.filter_get_lang` and
 /// the `settings.filter_map_lang` variables.
+#[cfg(feature = "lang-detection")]
 fn update_env_lang_detection(settings: &mut Settings) {
     if let Ok(env_var) = env::var(ENV_VAR_TPNOTE_LANG_DETECTION) {
         if env_var.is_empty() {
@@ -342,7 +351,7 @@ fn update_env_lang_detection(settings: &mut Settings) {
             let _ = mem::replace(&mut settings.filter_get_lang, FilterGetLang::Disabled);
             let _ = mem::replace(&mut settings.filter_map_lang_btmap, None);
             log::debug!(
-                "Empty env. var. `{}` disables `lang-detection` feature.",
+                "Empty env. var. `{}` disables the `lang-detection` feature.",
                 ENV_VAR_TPNOTE_LANG_DETECTION
             );
             return;
@@ -352,7 +361,6 @@ fn update_env_lang_detection(settings: &mut Settings) {
         let mut hm: BTreeMap<String, String> = BTreeMap::new();
         let mut all_languages_selected = false;
         match env_var
-            // The happy path.
             .split(',')
             .map(|t| {
                 let t = t.trim();
@@ -377,9 +385,9 @@ fn update_env_lang_detection(settings: &mut Settings) {
                     true
                 }
             })
-            // The error path.
             .map(|l| {
                 IsoCode639_1::from_str(l.trim()).map_err(|_| {
+                    // The error path.
                     // Produce list of all available langugages.
                     let mut all_langs = lingua::Language::all()
                         .iter()
@@ -401,6 +409,7 @@ fn update_env_lang_detection(settings: &mut Settings) {
             })
             .collect::<Result<Vec<IsoCode639_1>, ConfigError>>()
         {
+            // The happy path.
             Ok(mut iso_codes) => {
                 // Add the user's language subtag as reported from the OS.
                 // Continue the happy path.
@@ -429,11 +438,28 @@ fn update_env_lang_detection(settings: &mut Settings) {
                 }
                 let _ = mem::replace(&mut settings.filter_map_lang_btmap, Some(hm));
             }
+            // The error path.
             Err(e) =>
             // Store error.
             {
                 let _ = mem::replace(&mut settings.filter_get_lang, FilterGetLang::Error(e));
             }
+        }
+    }
+}
+
+/// Ignore the environment variable `LANG_DETECTION`.
+#[cfg(not(feature = "lang-detection"))]
+fn update_env_lang_detection(settings: &mut Settings) {
+    if let Ok(env_var) = env::var(ENV_VAR_TPNOTE_LANG_DETECTION) {
+        if !env_var.is_empty() {
+            let _ = mem::replace(&mut settings.filter_get_lang, FilterGetLang::Disabled);
+            let _ = mem::replace(&mut settings.filter_map_lang_btmap, None);
+            log::debug!(
+                "Ignoring the env. var. `{}`. The `lang-detection` feature \
+                 is not included in this build.",
+                ENV_VAR_TPNOTE_LANG_DETECTION
+            );
         }
     }
 }