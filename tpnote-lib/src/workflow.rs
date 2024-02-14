76ee70056473b97d21a8b8aeac67c9c6e8f52e77
Jens Getreu <getreu@web.de>
2024-02-14 07:25:40 +0200

    Workflow: add typestate builder API

--- tpnote-lib/src/workflow.rs
+++ tpnote-lib/src/workflow.rs
@@ -162,6 +162,218 @@ use std::path::PathBuf;
 use tera::Tera;
 use tera::Value;
 
+/// Typestate of the `WorkflowBuilder`.
+pub struct WorkflowBuilder<W> {
+    input: W,
+}
+
+/// In this state the workflow will only synchronize the filename.
+pub struct SyncFilename<'a> {
+    path: &'a Path,
+}
+
+/// In this state the workflow will either synchronize the filename of an
+/// existing note or, -if none exists- create a new note.
+pub struct SyncFilenameOrCreateNew<'a, T, F> {
+    scheme_new_default: &'a str,
+    path: &'a Path,
+    clipboard: &'a T,
+    stdin: &'a T,
+    tk_filter: F,
+    html_export: &'a Option<(PathBuf, LocalLinkKind)>,
+    force_scheme: Option<&'a str>,
+    force_lang: Option<&'a str>,
+}
+
+impl<'a> WorkflowBuilder<SyncFilename<'a>> {
+    /// Constructor of all workflows. The `path` points
+    /// 1. to an existing note file, or
+    /// 2. to a directory where the new note should be created, or
+    /// 3. to a non-Tp-Note file that will be annotated.
+    ///
+    /// For cases 2. and 3. upgrade the `WorkflowBuilder` with
+    /// `sync_or_create_new` to add additional input data.
+    pub fn new(path: &'a Path) -> Self {
+        Self {
+            input: SyncFilename { path },
+        }
+    }
+
+    /// Upgrade the `WorkflowBuilder` to enable also the creation of new note
+    /// files. It requires providing additinal input data:
+    ///
+    /// New notes are created by inserting `Tp-Note`'s environment
+    /// in a template. The template set being used, is detemined by
+    /// `scheme_new_default`. If the note to be created exists already, append
+    /// a so called `copy_counter` to the filename and try to save it again. In
+    /// case this does not succeed either, increment the `copy_counter` until a
+    /// free filename is found. The returned path points to the (new) note file
+    /// on disk. Depending on the context, Tp-Note chooses one `TemplateKind`
+    /// to operate (c.f. `tpnote_lib::template::TemplateKind::from()`).
+    /// The `tk-filter` allows to overwrite this choice, e.g. you may set
+    /// `TemplateKind::None` under certain circumstances. This way the caller
+    /// can disable the filename synchronization and inject behavior like
+    /// `--no-filename-sync`.
+    ///
+    /// Some templates insert the content of the clipboard or the standard
+    /// input pipe. The input data (can be empty) must be provided with the
+    /// parameters `clipboard` and `stdin`.
+    pub fn upgrade<T: Content, F: Fn(TemplateKind) -> TemplateKind>(
+        self,
+        scheme_new_default: &'a str,
+        clipboard: &'a T,
+        stdin: &'a T,
+        tk_filter: F,
+    ) -> WorkflowBuilder<SyncFilenameOrCreateNew<'a, T, F>> {
+        WorkflowBuilder {
+            input: SyncFilenameOrCreateNew {
+                path: self.input.path,
+                scheme_new_default,
+                clipboard,
+                stdin,
+                tk_filter,
+                html_export: &None,
+                force_scheme: None,
+                force_lang: None,
+            },
+        }
+    }
+
+    /// Starts the "synchronize filename" workflow. Errors can occur in
+    /// various ways, see `NoteError`.
+    ///
+    /// First, the workflow opens the note file `path` on disk and read its
+    /// YAML front matter. Then, it calculates from the front matter how the
+    /// filename should be to be in sync. If it is different, rename the note on
+    /// disk. Finally, it returns the note's new or existing filename. Repeated
+    /// calls, will reload the environment variables, but not the configuration
+    /// file. This function is stateless.
+    ///
+    ///
+    /// ## Example with `TemplateKind::SyncFilename`
+    ///
+    /// ```rust
+    /// use tpnote_lib::content::ContentString;
+    /// use tpnote_lib::workflow::WorkflowBuilder;
+    /// use std::env::temp_dir;
+    /// use std::fs;
+    /// use std::path::Path;
+    ///
+    /// // Prepare test: create existing note.
+    /// let raw = r#"
+    ///
+    /// ---
+    /// title: "My day"
+    /// subtitle: "Note"
+    /// ---
+    /// Body text
+    /// "#;
+    /// let notefile = temp_dir().join("20221030-hello.md");
+    /// fs::write(&notefile, raw.as_bytes()).unwrap();
+    ///
+    /// let expected = temp_dir().join("20221030-My day--Note.md");
+    /// let _ = fs::remove_file(&expected);
+    ///
+    /// // Build and run workflow.
+    /// let n = WorkflowBuilder::new(&notefile)
+    ///      // You can plug in your own type (must impl. `Content`).
+    ///      .run::<ContentString>()
+    ///      .unwrap();
+    ///
+    /// // Check result
+    /// assert_eq!(n, expected);
+    /// assert!(n.is_file());
+    /// ```
+    pub fn run<T: Content>(self) -> Result<PathBuf, NoteError> {
+        synchronize_filename::<T>(self.input.path)
+    }
+}
+
+impl<'a, T: Content, F: Fn(TemplateKind) -> TemplateKind>
+    WorkflowBuilder<SyncFilenameOrCreateNew<'a, T, F>>
+{
+    /// Set a flag, that the workflow also stores an HTML-rendition of the
+    /// note file next to it.
+    /// This optional HTML rendition is performed just before returning and does
+    /// not affect any above described operation.
+    pub fn html_export(&mut self, html_export: &'a Option<(PathBuf, LocalLinkKind)>) {
+        self.input.html_export = html_export;
+    }
+
+    /// Overwrite the default scheme.
+    pub fn force_scheme(&mut self, force_scheme: &'a str) {
+        self.input.force_scheme = Some(force_scheme);
+    }
+
+    /// By default, the natural language, the note is written in is guessed
+    /// from the title and subtitle. This disables the automatic guessing
+    /// and forces the language.
+    pub fn force_lang(&mut self, force_lang: &'a str) {
+        self.input.force_lang = Some(force_lang);
+    }
+
+    /// Starts the "synchronize filename or create a new note" workflow.
+    /// Returns the note's new or existing filename.  Repeated calls, will
+    /// reload the environment variables, but not  the configuration file. This
+    /// function is stateless.
+    /// Errors can occur in various ways, see `NoteError`.
+    ///
+    ///
+    /// ## Example with `TemplateKind::FromClipboard`
+    ///
+    /// ```rust
+    /// use tpnote_lib::content::Content;
+    /// use tpnote_lib::content::ContentString;
+    /// use tpnote_lib::workflow::WorkflowBuilder;
+    /// use std::env::temp_dir;
+    /// use std::path::PathBuf;
+    /// use std::fs;
+    ///
+    /// // Prepare test.
+    /// let notedir = temp_dir();
+    ///
+    /// let clipboard = ContentString::from("my clipboard\n".to_string());
+    /// let stdin = ContentString::from("my stdin\n".to_string());
+    /// // This is the condition to choose: `TemplateKind::FromClipboard`:
+    /// assert!(clipboard.header().is_empty() && stdin.header().is_empty());
+    /// assert!(!clipboard.body().is_empty() || !stdin.body().is_empty());
+    /// let template_kind_filter = |tk|tk;
+    ///
+    /// // Build and run workflow.
+    /// let n = WorkflowBuilder::new(&notedir)
+    ///       // You can plug in your own type (must impl. `Content`).
+    ///      .upgrade::<ContentString, _>(
+    ///            "default", &clipboard, &stdin, template_kind_filter)
+    ///      .run()
+    ///      .unwrap();
+    ///
+    /// // Check result.
+    /// assert!(n.as_os_str().to_str().unwrap()
+    ///    .contains("my stdin-my clipboard--Note"));
+    /// assert!(n.is_file());
+    /// let raw_note = fs::read_to_string(n).unwrap();
+    ///
+    /// #[cfg(not(target_family = "windows"))]
+    /// assert!(raw_note.starts_with(
+    ///            "\u{feff}---\ntitle:        |\n  my stdin\n  my clipboard"));
+    /// #[cfg(target_family = "windows")]
+    /// assert!(raw_note.starts_with(
+    ///            "\u{feff}---\r\ntitle:        |"));
+    /// ```
+    pub fn run(self) -> Result<PathBuf, NoteError> {
+        create_new_note_or_synchronize_filename::<T, F>(
+            self.input.path,
+            self.input.clipboard,
+            self.input.stdin,
+            self.input.tk_filter,
+            self.input.html_export,
+            self.input.scheme_new_default,
+            self.input.force_scheme,
+            self.input.force_lang,
+        )
+    }
+}
+
 /// Open the note file `path` on disk and read its YAML front matter.
 /// Then calculate from the front matter how the filename should be to
 /// be in sync. If it is different, rename the note on disk.
