getreu/tp-note

commit 37ef6eac32b566523417183646d86b96a5ab9afe

Change Html renderer API

tpnote/src/viewer/http_response.rs
@@ -14,7 +14,6 @@ use std::time::SystemTime;
 use tpnote_lib::config::LocalLinkKind;
 use tpnote_lib::config::LIB_CFG;
 use tpnote_lib::config::LIB_CFG_CACHE;
-use tpnote_lib::config::TMPL_HTML_VAR_DOC_ERROR;
 use tpnote_lib::config::TMPL_HTML_VAR_VIEWER_DOC_CSS_PATH_VALUE;
 use tpnote_lib::config::TMPL_HTML_VAR_VIEWER_DOC_JS;
 use tpnote_lib::config::TMPL_HTML_VAR_VIEWER_HIGHLIGHTING_CSS_PATH_VALUE;
@@ -420,12 +419,16 @@ impl HttpResponse for ServerThread {
 
         let mut context = self.context.clone();
 
-        if context.path != abspath_doc {
+        if context.path == abspath_doc {
+            // Save JavaScript code for life updates.
+            context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, &self.live_update_js);
+        } else {
             // This is not the base document, but some other Tp-Note document
             // we want to render. Store store its path.
             // `front_matter::assert_precondition()` needs this later.
             context.path = abspath_doc.to_path_buf();
             // Only the first base document is live updated.
+            // Overwrite the dynamic JavaScript.
             context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, "");
         }
         match HtmlRenderer::viewer_page::<ContentString>(context, content)
@@ -467,15 +470,14 @@ impl HttpResponse for ServerThread {
             // special error page and return this instead.
             Err(e) => {
                 // Render error page providing all information we havStringe.
-                let mut context = self.context.clone();
-                context.insert(TMPL_HTML_VAR_DOC_ERROR, &e.to_string());
+                let context = self.context.clone();
                 let note_erroneous_content = <ContentString as Content>::open(&context.path)?;
-                HtmlRenderer::error_page(context, note_erroneous_content).map_err(|e| {
-                    ViewerError::RenderErrorPage {
+                HtmlRenderer::error_page(context, note_erroneous_content, &e.to_string()).map_err(
+                    |e| ViewerError::RenderErrorPage {
                         tmpl: "tmpl_html.viewer_error".to_string(),
                         source: e,
-                    }
-                })
+                    },
+                )
             }
         }
     }

tpnote-lib/src/workflow.rs
@@ -138,6 +138,7 @@
 
 use crate::config::LocalLinkKind;
 use crate::config::LIB_CFG;
+use crate::config::TMPL_HTML_VAR_DOC_ERROR;
 #[cfg(feature = "viewer")]
 use crate::config::TMPL_HTML_VAR_DOC_TEXT;
 use crate::config::TMPL_VAR_CLIPBOARD;
@@ -595,7 +596,7 @@ impl HtmlRenderer {
     /// // We do not inject any JavaScript.
     /// context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, &"".to_string());
     /// // Render.
-    /// let html = HtmlRenderer::viewer::<ContentString>(context, raw.into())
+    /// let html = HtmlRenderer::viewer_page::<ContentString>(context, raw.into())
     ///            .unwrap();
     /// // Check the HTML rendition.
     /// assert!(html.starts_with("<!DOCTYPE html>\n<html"))
@@ -630,7 +631,7 @@ impl HtmlRenderer {
     /// // Render.
     /// let content = ContentString::open(&context.path).unwrap();
     /// // You can plug in your own type (must impl. `Content`).
-    /// let html = HtmlRenderer::viewer(context, content).unwrap();
+    /// let html = HtmlRenderer::viewer_page(context, content).unwrap();
     /// // Check the HTML rendition.
     /// assert!(html.starts_with("<!DOCTYPE html>\n<html"))
     /// ```
@@ -667,7 +668,7 @@ impl HtmlRenderer {
     /// let mut context = Context::from(Path::new("/path/to/note.md"));
     /// // The exporter template does not insert any JavaScript.
     /// // Render.
-    /// let html = HtmlRenderer::exporter::<ContentString>(context, raw.into())
+    /// let html = HtmlRenderer::exporter_page::<ContentString>(context, raw.into())
     ///            .unwrap();
     /// // Check the HTML rendition.
     /// assert!(html.starts_with("<!DOCTYPE html>\n<html"))
@@ -694,8 +695,7 @@ impl HtmlRenderer {
     /// `parse_hyperlinks::renderer::text_rawlinks2html` and inserted in
     /// the `TMPL_HTML_VIEWER_ERROR` template (can be replace at runtime).
     /// This template expects the template variables `TMPL_VAR_PATH`
-    /// and `TMPL_HTML_VAR_VIEWER_DOC_JS` and `TMPL_HTML_VAR_NOTE_ERROR` in
-    /// `context` to be set.
+    /// and `TMPL_HTML_VAR_VIEWER_DOC_JS` in `context` to be set.
     /// NB: The value of `TMPL_VAR_PATH` equals `context.path`.
     ///
     /// ```rust
@@ -725,15 +725,13 @@ impl HtmlRenderer {
     /// // Start test
     /// let mut context = Context::from(&notefile);
     /// // We do not inject any JavaScript.
-    /// context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, &e.to_string());
-    /// // We simulate an error;
-    /// context.insert(TMPL_HTML_VAR_DOC_ERROR, &e.to_string());
+    /// context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, "");
     /// // Render.
     /// // Read from file.
     /// // You can plug in your own type (must impl. `Content`).
     /// let content = ContentString::open(&context.path).unwrap();
     /// let html = HtmlRenderer::error_page(
-    ///               context, content).unwrap();
+    ///               context, content, &e.to_string()).unwrap();
     /// // Check the HTML rendition.
     /// assert!(html.starts_with("<!DOCTYPE html>\n<html"))
     /// ```
@@ -741,9 +739,10 @@ impl HtmlRenderer {
     pub fn error_page<T: Content>(
         mut context: Context,
         note_erroneous_content: T,
+        error_message: &str,
     ) -> Result<String, NoteError> {
         // Insert.
-
+        context.insert(TMPL_HTML_VAR_DOC_ERROR, error_message);
         context.insert(TMPL_HTML_VAR_DOC_TEXT, &note_erroneous_content.as_str());
 
         let tmpl_html = &LIB_CFG.read_recursive().tmpl_html.viewer_error;

tpnote/src/viewer/sse_server.rs
@@ -19,7 +19,6 @@ use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::SystemTime;
-use tpnote_lib::config::TMPL_HTML_VAR_VIEWER_DOC_JS;
 use tpnote_lib::context::Context;
 
 /// The TCP stream is read in chunks. This is the read buffer size.
@@ -72,6 +71,7 @@ pub fn manage_connections(
     // Store `doc_path` in the `context.path` and
     // in the Tera variable `TMPL_VAR_PATH`.
     let context = Context::from(&doc_path);
+    //
 
     log::info!(
         "Viewer listens to incomming requests.\n\
@@ -103,7 +103,6 @@ pub fn manage_connections(
                     let delivered_tpnote_docs = delivered_tpnote_docs.clone();
                     let conn_counter = conn_counter.clone();
                     let context = context.clone();
-
                     move || {
                         let mut st = ServerThread::new(
                             event_rx,
@@ -145,6 +144,10 @@ pub(crate) struct ServerThread {
     /// and in the Tera variable `TMPL_VAR_PATH`.
     /// Both are needed for rendering to HTML.
     pub(crate) context: Context,
+    /// Java Script injection code used by the root page for live updates.
+    /// Root pages insert this in their context with the key
+    /// `TMPL_HTML_VAR_VIEWR_DOC_JS`.
+    pub(crate) live_update_js: String,
 }
 
 impl ServerThread {
@@ -155,12 +158,12 @@ impl ServerThread {
         allowed_urls: Arc<RwLock<HashSet<PathBuf>>>,
         delivered_tpnote_docs: Arc<RwLock<HashSet<PathBuf>>>,
         conn_counter: Arc<()>,
-        mut context: Context,
+        context: Context,
     ) -> Self {
         let local_addr = stream.local_addr();
 
         // Compose JavaScript code.
-        let note_js = match local_addr {
+        let live_update_js = match local_addr {
             Ok(addr) => format!(
                 "{}{}:{}{}",
                 SSE_CLIENT_CODE1,
@@ -173,9 +176,6 @@ impl ServerThread {
             }
         };
 
-        // Save JavaScript code.
-        context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, &note_js);
-
         Self {
             rx,
             stream,
@@ -183,6 +183,7 @@ impl ServerThread {
             delivered_tpnote_docs,
             conn_counter,
             context,
+            live_update_js,
         }
     }
 

commit bf19d708ecc32baab20e69d4d43b7f63e48c11d6

Fix regression: insert `TMPL_HTML_VAR_VIEWER_DOC_JS` in error page
This fixes a regression introduced with commit 37ef6ea.

tpnote/src/viewer/http_response.rs
@@ -418,7 +418,6 @@ impl HttpResponse for ServerThread {
         let root_path = &self.context.root_path;
 
         let mut context = self.context.clone();
-
         if context.path == abspath_doc {
             // Save JavaScript code for life updates.
             context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, &self.live_update_js);
@@ -426,11 +425,14 @@ impl HttpResponse for ServerThread {
             // This is not the base document, but some other Tp-Note document
             // we want to render. Store store its path.
             // `front_matter::assert_precondition()` needs this later.
+            // Also, the HTML template expects this to be set to the rendered
+            // document.
             context.path = abspath_doc.to_path_buf();
             // Only the first base document is live updated.
             // Overwrite the dynamic JavaScript.
             context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, "");
         }
+
         match HtmlRenderer::viewer_page::<ContentString>(context, content)
             // Now scan the HTML result for links and store them in a Map
             // accessible to all threads.
@@ -469,8 +471,22 @@ impl HttpResponse for ServerThread {
             // We could not render the note properly. Instead we will render a
             // special error page and return this instead.
             Err(e) => {
-                // Render error page providing all information we havStringe.
-                let context = self.context.clone();
+                // Render error page providing all information we have.
+                let mut context = self.context.clone();
+                if context.path == abspath_doc {
+                    // Save JavaScript code for life updates.
+                    context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, &self.live_update_js);
+                } else {
+                    // This is not the base document, but some other Tp-Note
+                    // document we want to render. Store store its path. The
+                    // HTML template expects this to be set to the rendered
+                    // document.
+                    context.path = abspath_doc.to_path_buf();
+                    // Only the first base document is live updated.
+                    // Overwrite the dynamic JavaScript.
+                    context.insert(TMPL_HTML_VAR_VIEWER_DOC_JS, "");
+                }
+
                 let note_erroneous_content = <ContentString as Content>::open(&context.path)?;
                 HtmlRenderer::error_page(context, note_erroneous_content, &e.to_string()).map_err(
                     |e| ViewerError::RenderErrorPage {