5182c9b55bcc83ec7f0b881a1eb25b76abe07e68
Jens Getreu <getreu@web.de>
2024-03-19 14:03:43 +0200

    Add `shell.nix`

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ 
+    rustc 
+    cargo
+    cargo-edit 
+    gcc 
+    rustfmt 
+    clippy 
+    rust-analyzer
+  ];
+
+  # Certain Rust tools won't work without this
+  # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
