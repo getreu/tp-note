40214cb84103d55c25e153d61e015920118212e5
Jens Getreu <getreu@web.de>
2024-11-07 04:00:56 +0200

    Migrate to flake: `nix develop`

---
+++ flake.nix
@@ -0,0 +1,46 @@
+# Run with `nix develop` 
+
+{
+  description = "A Hunspell-like spell-checking library in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    { nixpkgs, rust-overlay, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forEachSystem = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShell = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            (toolchain.override {
+              extensions = [
+                "rust-src"
+                "clippy"
+                "llvm-tools-preview"
+              ];
+            })
+            rust-analyzer
+            cargo-flamegraph
+            cargo-edit
+            #cargo-llvm-cov
+            valgrind
+          ];
+          RUST_BACKTRACE = "1";
+        }
+      );
+    };
+}
