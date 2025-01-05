getreu/tp-note

commit d79a706992fc4ea95afcc8a88f982d481b794a35

Centralize HTML stream tagging code

tpnote-lib/src/filter.rs
@@ -6,6 +6,7 @@ use crate::config::TMPL_VAR_FM_;
 use crate::filename::NotePath;
 use crate::filename::NotePathBuf;
 use crate::filename::NotePathStr;
+use crate::html::HtmlStream;
 use crate::markup_language::InputConverter;
 use crate::markup_language::MarkupLanguage;
 #[cfg(feature = "lang-detection")]
@@ -34,12 +35,6 @@ use tera::{try_get_value, Result as TeraResult, Tera, Value};
 const TRUNC_LEN_MAX: usize = 200;
 #[cfg(test)]
 pub const TRUNC_LEN_MAX: usize = 10;
-
-/// Lowercase pattern to detect HTML in stdin.
-const HTML_PAT1: &str = "<!doctype html";
-
-/// Lowercase pattern to detect HTML in stdin.
-const HTML_PAT2: &str = "<html";
 
 /// Tera object with custom functions registered.
 pub static TERA: LazyLock<Tera> = LazyLock::new(|| {
@@ -304,7 +299,7 @@ fn html_to_markup_filter<S: BuildHasher>(
         .lines()
         .next()
         .map(|l| l.trim_start().to_ascii_lowercase());
-    if firstline.is_some_and(|l| l.starts_with(HTML_PAT1) || l.starts_with(HTML_PAT2)) {
+    if firstline.is_some_and(|l| HtmlStream::has_start_tag(&l)) {
         let extension = if let Some(ext) = args.get("extension") {
             try_get_value!("markup_to_html", "extension", String, ext)
         } else {

tpnote-lib/src/html.rs
@@ -1,5 +1,6 @@
 //! Helper functions dealing with HTML conversion.
 use crate::clone_ext::CloneExt;
+use crate::error::InputStreamError;
 use crate::filename::{NotePath, NotePathStr};
 use crate::{config::LocalLinkKind, error::NoteError};
 use html_escape;
@@ -763,6 +764,60 @@ pub fn rewrite_links(
 
     html_out
     // The `RwLockWriteGuard` is released here.
+}
+
+pub struct HtmlStream;
+
+impl HtmlStream {
+    /// Pattern 1 to check if this is HTML.
+    const START_TAG_PAT1: &'static str = "<html";
+    /// Pattern 1 to check if this is HTML.
+    const START_TAG_PAT2: &'static str = "<!doctype html";
+    /// Lowercase test pattern to check if there is a document type declaration
+    /// already.
+    const START_TAG_PAT3: &'static str = "<!doctype ";
+    /// Tag to insert if there is no start tag.
+    const START_TAG: &'static str = "<!DOCTYPE html>";
+    /// Return `true` when the input stream starts with  `<!DOCTYPE html`
+    /// or `<html` (or lowercase variants).
+    pub fn has_start_tag(html: &str) -> bool {
+        let html2 = html
+            .trim_start()
+            .lines()
+            .next()
+            .map(|l| l.to_ascii_lowercase());
+        html2.as_ref().is_some_and(|l| {
+            l.starts_with(Self::START_TAG_PAT1) || l.starts_with(Self::START_TAG_PAT2)
+        })
+    }
+
+    /// If the input does not start with `<!DOCTYPE html` or `<html`
+    /// (or lowercase variants), then insert `<!DOCTYPE html>`.
+    /// Returns `InputStreamError::NonHtmlDoctype` if there is another Doctype
+    /// already.
+    pub fn prepend_start_tag(html: String) -> Result<String, InputStreamError> {
+        let html2 = html
+            .trim_start()
+            .lines()
+            .next()
+            .map(|l| l.to_ascii_lowercase());
+
+        if html2.as_ref().is_some_and(|l| {
+            l.starts_with(Self::START_TAG_PAT1) || l.starts_with(Self::START_TAG_PAT2)
+        }) {
+            // There is an HTML tag already. Nothing to do.
+            return Ok(html);
+        }
+
+        if html2.is_some_and(|l| l.starts_with(Self::START_TAG_PAT3)) {
+            // There is a Doctype other then HTML.
+            return Err(InputStreamError::NonHtmlDoctype);
+        }
+
+        let mut html = html;
+        html.insert_str(0, Self::START_TAG);
+        Ok(html)
+    }
 }
 
 #[cfg(test)]

tpnote/src/clipboard.rs
@@ -15,14 +15,6 @@ use wl_clipboard_rs::copy;
 #[cfg(unix)]
 use wl_clipboard_rs::paste;
 
-/// Lowercase test pattern to check if there is a document type declaration
-/// already.
-#[cfg(feature = "read-clipboard")]
-const HTML_PAT1: &str = "<!doctype ";
-/// Prepend a marker declaration, in case the content is HTML.
-#[cfg(feature = "read-clipboard")]
-const HTML_PAT2: &str = "<!DOCTYPE html>";
-
 #[derive(Default, Debug, PartialEq, Eq)]
 pub struct SystemClipboard {
     pub html: ContentString,
@@ -35,7 +27,7 @@ impl SystemClipboard {
     /// prepended.
     #[cfg(feature = "read-clipboard")]
     pub(crate) fn new() -> Self {
-        use tpnote_lib::content::Content;
+        use tpnote_lib::{content::Content, html::HtmlStream};
 
         let mut txt_content = String::new();
         let mut html_content = String::new();
@@ -51,15 +43,13 @@ impl SystemClipboard {
             ) {
                 match pipe_reader.read_to_string(&mut html_content) {
                     Ok(l) if l > 0 => {
-                        if !html_content.trim_start().is_empty()
-                            && !html_content
-                                .lines()
-                                .next()
-                                .map(|l| l.trim_start().to_ascii_lowercase())
-                                .is_some_and(|l| l.starts_with(HTML_PAT1))
-                        {
-                            html_content.insert_str(0, HTML_PAT2);
-                        }
+                        html_content = HtmlStream::prepend_start_tag(html_content)
+                            .map_err(|e| {
+                                log::warn!("HTML Wayland clipboard: {}", e);
+                                e
+                            })
+                            // Ignore error and continue with empty string.
+                            .unwrap_or_default();
                     }
                     _ => {}
                 }
@@ -77,17 +67,14 @@ impl SystemClipboard {
         if html_content.is_empty() && txt_content.is_empty() {
             // Query X11 clipboard.
             if let Ok(ctx) = ClipboardContext::new() {
-                if let Ok(mut html) = ctx.get_html() {
-                    if !html.trim_start().is_empty()
-                        && !html
-                            .lines()
-                            .next()
-                            .map(|l| l.trim_start().to_ascii_lowercase())
-                            .is_some_and(|l| l.starts_with(HTML_PAT1))
-                    {
-                        html.insert_str(0, HTML_PAT2);
-                    }
-                    html_content = html;
+                if let Ok(html) = ctx.get_html() {
+                    html_content = HtmlStream::prepend_start_tag(html)
+                        .map_err(|e| {
+                            log::warn!("HTML X11 clipboard: {}", e);
+                            e
+                        })
+                        // Ignore error and continue with empty string.
+                        .unwrap_or_default();
                 };
                 if let Ok(txt) = ctx.get_text() {
                     txt_content = txt;

tpnote-lib/src/error.rs
@@ -8,6 +8,19 @@ use thiserror::Error;
 /// note file. This constant limits the number of text lines that are printed.
 pub const FRONT_MATTER_ERROR_MAX_LINES: usize = 20;
 
+/// Error related the clipboard or Stdin input stream.
+#[derive(Debug, Error)]
+pub enum InputStreamError {
+    /// Remedy: Prepend HTML input data with `<!DOCTYPE html>` or `<html>`.
+    /// with a doctype other than `<!DOCTYPE html>`.
+    #[error(
+        "The HTML input stream from the clipboard or\n\
+         from stdin starts with a doctype other than\n\
+         \"<!DOCTYPE html>\"."
+    )]
+    NonHtmlDoctype,
+}
+
 /// Configuration file related filesystem and syntax errors.
 #[derive(Debug, Error)]
 pub enum FileError {

commit 645c592596c6f4dfccf343002268002e8361a4ad

Regr. fix: Empty HTML clipboard contains `<!DOCTYPE html>`
This fixes a regression introduced with commit:

    d79a706  Centralize HTML stream tagging code

Before the HTML clipboard data `<!DOCTYPE html>` was considered as non-empty
clipboard.

tpnote-lib/src/content.rs
@@ -10,8 +10,6 @@ use std::io::Write;
 use std::path::Path;
 use substring::Substring;
 
-use crate::html::HtmlStream;
-
 /// As all text before the header marker `"---"` is ignored, this
 /// constant limits the maximum number of characters that are skipped
 /// before the header starts. In other words: the header
@@ -240,6 +238,8 @@ pub trait Content: AsRef<str> + Debug + Eq + PartialEq + Default + From<String> 
     /// 2. followed by header bytes,
     /// 3. same as above ...
     fn split(content: &str) -> (&str, &str) {
+        // Bring in scope `HtmlString`.
+        use crate::html::HtmlStr;
         // Remove BOM
         let content = content.trim_start_matches('\u{feff}');
 
@@ -251,7 +251,7 @@ pub trait Content: AsRef<str> + Debug + Eq + PartialEq + Default + From<String> 
         // TODO: In the future the header might be constructed from
         // the "meta" HTML fields. Though I am not sure if something meaningful
         // can be found in HTML clipboard meta data.
-        if HtmlStream::has_start_tag(content) {
+        if content.has_html_start_tag() {
             return ("", content);
         }
 

tpnote-lib/src/filter.rs
@@ -6,7 +6,6 @@ use crate::config::TMPL_VAR_FM_;
 use crate::filename::NotePath;
 use crate::filename::NotePathBuf;
 use crate::filename::NotePathStr;
-use crate::html::HtmlStream;
 use crate::markup_language::InputConverter;
 use crate::markup_language::MarkupLanguage;
 #[cfg(feature = "lang-detection")]
@@ -286,6 +285,9 @@ fn html_to_markup_filter<S: BuildHasher>(
     value: &Value,
     #[allow(unused_variables)] args: &HashMap<String, Value, S>,
 ) -> TeraResult<Value> {
+    // Bring new methods into scope.
+    use crate::html::HtmlStr;
+
     #[allow(unused_mut)]
     let mut buffer = try_get_value!("html_to_markup", "value", String, value);
 
@@ -299,7 +301,7 @@ fn html_to_markup_filter<S: BuildHasher>(
         .lines()
         .next()
         .map(|l| l.trim_start().to_ascii_lowercase());
-    if firstline.is_some_and(|l| HtmlStream::has_start_tag(&l)) {
+    if firstline.is_some_and(|l| l.as_str().has_html_start_tag()) {
         let extension = if let Some(ext) = args.get("extension") {
             try_get_value!("markup_to_html", "extension", String, ext)
         } else {

tpnote-lib/src/html.rs
@@ -766,67 +766,112 @@ pub fn rewrite_links(
     // The `RwLockWriteGuard` is released here.
 }
 
-/// This trait deals with tagged HTML data.
-pub struct HtmlStream;
+/// This trait deals with tagged HTML `&str` data.
+pub trait HtmlStr {
+    /// Lowercase pattern to check if this is a Doctype tag.
+    const TAG_DOCTYPE_PAT: &'static str = "<!doctype";
+    /// Lowercase pattern to check if this Doctype is HTML.
+    const TAG_DOCTYPE_HTML_PAT: &'static str = "<!doctype html";
+    /// Doctype HTML tag. This is inserted by
+    /// `<HtmlString>.prepend_html_start_tag()`
+    const TAG_DOCTYPE_HTML: &'static str = "<!DOCTYPE html>";
+    /// Pattern to check if f this is an HTML start tag.
+    const START_TAG_HTML_PAT: &'static str = "<html";
+    /// HTML end tag.
+    const END_TAG_HTML: &'static str = "</html>";
 
-impl HtmlStream {
-    /// Pattern 1 to check if this is HTML.
-    const START_TAG_PAT1: &'static str = "<html";
-    /// Pattern 2 to check if this is HTML.
-    const START_TAG_PAT2: &'static str = "<!doctype html";
-    /// Lowercase test pattern to check if there is a document type declaration
-    /// already.
-    const START_TAG_PAT3: &'static str = "<!doctype ";
-    /// Tag to insert if there is no start tag.
-    const START_TAG: &'static str = "<!DOCTYPE html>";
-    /// Return `true` when the input stream starts with  `<!DOCTYPE html`
-    /// or `<html` (or lowercase variants).
-    pub fn has_start_tag(html: &str) -> bool {
-        let html2 = html
+    /// We consider `self` empty, when it equals to `<!DOCTYPE html...>` or
+    /// when it is empty.
+    fn is_empty_html(&self) -> bool;
+
+    /// We consider `html` empty, when it equals to `<!DOCTYPE html...>` or
+    /// when it is empty.
+    /// This is identical to `is_empty_html()`, but does not pull in
+    /// additional trait bounds.
+    fn is_empty_html2(html: &str) -> bool {
+        html.is_empty_html()
+    }
+
+    /// True if stream starts with `<!DOCTYPE html...>`.
+    fn has_html_start_tag(&self) -> bool;
+
+    /// True if `html` starts with `<!DOCTYPE html...>`.
+    /// This is identical to `has_html_start_tag()`, but does not pull in
+    /// additional trait bounds.
+    fn has_html_start_tag2(html: &str) -> bool {
+        html.has_html_start_tag()
+    }
+}
+
+impl HtmlStr for str {
+    fn is_empty_html(&self) -> bool {
+        let html = self
+            .trim_start()
+            .lines()
+            .next()
+            .map(|l| l.to_ascii_lowercase())
+            .unwrap_or_default();
+        html.is_empty()
+            || (html.as_str().starts_with(Self::TAG_DOCTYPE_HTML_PAT)
+            // The next closing braket must be in last position.
+         && html.find('>').unwrap_or_default() == html.len()-1)
+    }
+
+    fn has_html_start_tag(&self) -> bool {
+        let html = self
             .trim_start()
             .lines()
             .next()
             .map(|l| l.to_ascii_lowercase());
-        html2.as_ref().is_some_and(|l| {
-            l.starts_with(Self::START_TAG_PAT1) || l.starts_with(Self::START_TAG_PAT2)
-        })
+        html.as_ref()
+            .is_some_and(|l| l.starts_with(Self::TAG_DOCTYPE_HTML_PAT))
     }
+}
 
-    /// If the input does not start with `<!DOCTYPE html` or `<html`
+pub trait HtmlString: Sized {
+    /// If the input does not start with `<!DOCTYPE html`
     /// (or lowercase variants), then insert `<!DOCTYPE html>`.
     /// Returns `InputStreamError::NonHtmlDoctype` if there is another Doctype
     /// already.
-    pub fn prepend_start_tag(html: String) -> Result<String, InputStreamError> {
-        let html2 = html
+    fn prepend_html_start_tag(self) -> Result<Self, InputStreamError>;
+}
+
+/// This trait deals with tagged HTML `String` data.
+impl HtmlString for String {
+    fn prepend_html_start_tag(self) -> Result<Self, InputStreamError> {
+        // Bring `HtmlStr` methods into scope.
+        use crate::html::HtmlStr;
+
+        let html2 = self
             .trim_start()
             .lines()
             .next()
-            .map(|l| l.to_ascii_lowercase());
+            .map(|l| l.to_ascii_lowercase())
+            .unwrap_or_default();
 
-        if html2.as_ref().is_some_and(|l| {
-            l.starts_with(Self::START_TAG_PAT1) || l.starts_with(Self::START_TAG_PAT2)
-        }) {
-            // There is an HTML tag already. Nothing to do.
-            return Ok(html);
-        }
-
-        if html2.is_some_and(|l| l.starts_with(Self::START_TAG_PAT3)) {
+        if html2.starts_with(<str as HtmlStr>::TAG_DOCTYPE_HTML_PAT) {
+            // Has a start tag already.
+            Ok(self)
+        } else if !html2.starts_with(<str as HtmlStr>::TAG_DOCTYPE_PAT) {
+            // Insert HTML Doctype tag.
+            let mut html = self;
+            html.insert_str(0, <str as HtmlStr>::TAG_DOCTYPE_HTML);
+            Ok(html)
+        } else {
             // There is a Doctype other then HTML.
-            return Err(InputStreamError::NonHtmlDoctype);
+            Err(InputStreamError::NonHtmlDoctype)
         }
-
-        let mut html = html;
-        html.insert_str(0, Self::START_TAG);
-        Ok(html)
     }
 }
 
 #[cfg(test)]
 mod tests {
 
+    use crate::error::InputStreamError;
     use crate::error::NoteError;
     use crate::html::assemble_link;
     use crate::html::rewrite_links;
+    use crate::html::Hyperlink;
     use parking_lot::RwLock;
     use parse_hyperlinks::parser::Link;
     use parse_hyperlinks_extras::parser::parse_html::take_link;
@@ -836,47 +881,6 @@ mod tests {
         path::{Path, PathBuf},
         sync::Arc,
     };
-
-    use super::*;
-
-    #[test]
-    fn test_has_start_tag() {
-        // Test cases where the start tag is present.
-        assert!(HtmlStream::has_start_tag("<html>"));
-        assert!(HtmlStream::has_start_tag("<!DOCTYPE html>"));
-        assert!(HtmlStream::has_start_tag("   <html>"));
-        assert!(HtmlStream::has_start_tag("   <!DOCTYPE html>"));
-
-        // Test cases where the start tag is not present.
-        assert!(!HtmlStream::has_start_tag("<!DOCTYPE other>"));
-        assert!(!HtmlStream::has_start_tag("<body>"));
-        assert!(!HtmlStream::has_start_tag("   <body>"));
-    }
-
-    #[test]
-    fn test_prepend_start_tag() {
-        // Test case where the start tag is already present.
-        assert_eq!(
-            HtmlStream::prepend_start_tag("<html>".to_string()).unwrap(),
-            "<html>".to_string()
-        );
-        assert_eq!(
-            HtmlStream::prepend_start_tag("<!DOCTYPE html>".to_string()).unwrap(),
-            "<!DOCTYPE html>".to_string()
-        );
-
-        // Test case where the start tag needs to be inserted.
-        assert_eq!(
-            HtmlStream::prepend_start_tag("<body>".to_string()).unwrap(),
-            "<!DOCTYPE html><body>".to_string()
-        );
-
-        // Test case where there is another doctype.
-        assert_eq!(
-            HtmlStream::prepend_start_tag("<!DOCTYPE other>".to_string()).unwrap_err(),
-            InputStreamError::NonHtmlDoctype
-        );
-    }
 
     #[test]
     fn test_assemble_link() {
@@ -1721,5 +1725,118 @@ mod tests {
         println!("{:?}", allowed_urls.read_recursive());
         assert!(url.contains(&PathBuf::from("/abs/note path/")));
         assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_is_empty_html() {
+        // Bring new methods into scope.
+        use crate::html::HtmlStr;
+
+        // Test where input is '<!DOCTYPE html>'
+        // See: [HTML doctype declaration](https://www.w3schools.com/tags/tag_doctype.ASP)
+        assert!(String::from("<!DOCTYPE html>").is_empty_html());
+
+        // This should fail:
+        assert!(!String::from("<!DOCTYPE html>>").is_empty_html());
+
+        // Test where input is '<!DOCTYPE html>'
+        // See: [HTML doctype declaration](https://www.w3schools.com/tags/tag_doctype.ASP)
+        assert!(String::from(
+            " <!DOCTYPE HTML PUBLIC \
+            \"-//W3C//DTD HTML 4.01 Transitional//EN\" \
+            \"http://www.w3.org/TR/html4/loose.dtd\">"
+        )
+        .is_empty_html());
+
+        // Test where input is '<!DOCTYPE html>'
+        // See: [HTML doctype declaration](https://www.w3schools.com/tags/tag_doctype.ASP)
+        assert!(String::from(
+            " <!DOCTYPE html PUBLIC \
+            \"-//W3C//DTD XHTML 1.1//EN\" \
+            \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">"
+        )
+        .is_empty_html());
+
+        // Test where input is '<!DOCTYPE html>Some content'
+        assert!(!String::from("<!DOCTYPE html>Some content").is_empty_html());
+
+        // Test where input is an empty string
+        assert!(String::from("").is_empty_html());
+
+        // Test where input is not empty HTML.
+        // Convention: we consider empty only `` or `<!DOCTYPE html>`.
+        assert!(!String::from("<html></html>").is_empty_html());
+
+        // Test where input is not empty HTML with doctype
+        // Convention: we consider empty only `` or `<!DOCTYPE html>`.
+        assert!(!String::from("<!DOCTYPE html><html></html>").is_empty_html());
+    }
+
+    #[test]
+    fn test_has_html_start_tag() {
+        // Bring new methods into scope.
+        use crate::html::HtmlStr;
+
+        // Test where input is '<!DOCTYPE html>Some content'
+        assert!(String::from("<!DOCTYPE html>Some content").has_html_start_tag());
+
+        // This fails because we require be convention `<!DOCTYPE html>` as
+        // first tag
+        assert!(!String::from("<html>Some content</html>").has_html_start_tag());
+
+        // This fails because we require be convention `<!DOCTYPE html>` as
+        // first tag
+        assert!(!String::from("<HTML>").has_html_start_tag());
+
+        // Test where input starts with spaces
+        assert!(String::from("  <!doctype html>Some content").has_html_start_tag());
+
+        // Test where input is a non-HTML doctype
+        assert!(!String::from("<!DOCTYPE other>").has_html_start_tag());
+
+        // Test where input is an empty string
+        assert!(!String::from("").has_html_start_tag());
+    }
+
+    #[test]
+    fn test_prepend_html_start_tag() {
+        // Bring new methods into scope.
+        use crate::html::HtmlString;
+
+        // Test where input already has doctype HTML
+        assert_eq!(
+            String::from("<!DOCTYPE html>Some content").prepend_html_start_tag(),
+            Ok(String::from("<!DOCTYPE html>Some content"))
+        );
+
+        // Test where input already has doctype HTML
+        assert_eq!(
+            String::from("<!DOCTYPE html>").prepend_html_start_tag(),
+            Ok(String::from("<!DOCTYPE html>"))
+        );
+
+        // Test where input has no HTML tag
+        assert_eq!(
+            String::from("<html>Some content").prepend_html_start_tag(),
+            Ok(String::from("<!DOCTYPE html><html>Some content"))
+        );
+
+        // Test where input has a non-HTML doctype
+        assert_eq!(
+            String::from("<!DOCTYPE other>").prepend_html_start_tag(),
+            Err(InputStreamError::NonHtmlDoctype)
+        );
+
+        // Test where input has no HTML tag
+        assert_eq!(
+            String::from("Some content").prepend_html_start_tag(),
+            Ok(String::from("<!DOCTYPE html>Some content"))
+        );
+
+        // Test where input is an empty string
+        assert_eq!(
+            String::from("").prepend_html_start_tag(),
+            Ok(String::from("<!DOCTYPE html>"))
+        );
     }
 }

tpnote-lib/src/template.rs
@@ -1,5 +1,6 @@
 //!Abstractions for content templates and filename templates.
 use crate::filename::NotePath;
+use crate::html::HtmlStr;
 use crate::settings::SETTINGS;
 use crate::{config::LIB_CFG, content::Content};
 use std::path::Path;
@@ -44,7 +45,8 @@ impl TemplateKind {
         let stdin_is_empty = stdin.is_empty();
         let stdin_has_header = !stdin.header().is_empty();
 
-        let clipboard_is_empty = html_clipboard.is_empty() && txt_clipboard.is_empty();
+        let clipboard_is_empty =
+            <str as HtmlStr>::is_empty_html2(html_clipboard.as_str()) && txt_clipboard.is_empty();
         let clipboard_has_header =
             !html_clipboard.header().is_empty() || !txt_clipboard.header().is_empty();
 

tpnote/Cargo.toml
@@ -92,8 +92,8 @@ thiserror.workspace = true
 time = "0.3.37"
 tera.workspace = true
 toml.workspace = true
-#tpnote-lib = { path = "../tpnote-lib", default-features = false }
-tpnote-lib = { version = "0.38.0", default-features = false}
+tpnote-lib = { path = "../tpnote-lib", default-features = false }
+#tpnote-lib = { version = "0.38.0", default-features = false}
 webbrowser = { version = "1.0.3", optional = true }
 
 

tpnote/src/clipboard.rs
@@ -27,7 +27,9 @@ impl SystemClipboard {
     /// prepended.
     #[cfg(feature = "read-clipboard")]
     pub(crate) fn new() -> Self {
-        use tpnote_lib::{content::Content, html::HtmlStream};
+        // Bring new methods into scope.
+        use tpnote_lib::content::Content;
+        use tpnote_lib::html::HtmlString;
 
         let mut txt_content = String::new();
         let mut html_content = String::new();
@@ -43,7 +45,8 @@ impl SystemClipboard {
             ) {
                 match pipe_reader.read_to_string(&mut html_content) {
                     Ok(l) if l > 0 => {
-                        html_content = HtmlStream::prepend_start_tag(html_content)
+                        html_content = html_content
+                            .prepend_html_start_tag()
                             .map_err(|e| {
                                 log::warn!("HTML Wayland clipboard: {}", e);
                                 e
@@ -68,7 +71,8 @@ impl SystemClipboard {
             // Query X11 clipboard.
             if let Ok(ctx) = ClipboardContext::new() {
                 if let Ok(html) = ctx.get_html() {
-                    html_content = HtmlStream::prepend_start_tag(html)
+                    html_content = html
+                        .prepend_html_start_tag()
                         .map_err(|e| {
                             log::warn!("HTML X11 clipboard: {}", e);
                             e